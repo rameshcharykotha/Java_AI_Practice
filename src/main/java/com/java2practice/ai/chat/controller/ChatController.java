03dbba03e7d10f7ae773d6f00a7f01a2fb15aa2e
rameshcharykotha <rameshcharykotha@gmail.com>
2025-05-18 22:17:10 -0500

    Spring boot integration with OpenAI

---
+++ src/main/java/com/java2practice/ai/chat/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.java2practice.ai.chat.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    @Autowired
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+}
---
+++ src/test/java/com/java2practice/ai/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java2practice.ai.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
